363b203c370ed411918dc9758a8b54821cafc5a6
Keisuke Nakayama <me@yammer.jp>
2024-03-31 17:27:31 +0900

    add 5 animals

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_31_082510) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_31_082715) do
   create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chicks", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hedgehogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "owls", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
