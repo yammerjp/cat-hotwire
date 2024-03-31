f2b35ddb34fbba434c3d970268705ef2b5a5228a
Keisuke Nakayama <me@yammer.jp>
2024-03-31 17:48:30 +0900

    inline validation

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem "kaminari"
 
 gem "ransack"
 
+gem "bootstrap_form"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,8 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.ransackable_attributes(auth_object = nil)
     ["age", "created_at", "id", "id_value", "name", "updated_at"]
   end
